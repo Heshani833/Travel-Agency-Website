e6b34c3819bc1d8f88ccc7cef297f4938e7a4990
Heshani833 <heshanishehana123@gmail.com>
2026-03-13 11:16:29 +0530

    backend to use Flask and implement contact route

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from flask import Flask
+from flask_cors import CORS 
 
-app = FastAPI()
+from routes.contact_routes import contact_bp
+from routes.auth_routes import auth_bp
 
-@app.get("/")
+app = Flask(__name__)
+CORS(app)
+
+#register routes
+app.register_blueprint(contact_bp)
+app.register_blueprint(auth_bp)
+
+@app.route("/")
 def home():
-    return {"message": "Travel Agency Backend Running"}
+    return {"message":"Travel Agency Backend Running"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
