c6270cdee851041fbb951b38f5ff49bd9966274f
Heshani833 <heshanishehana123@gmail.com>
2026-03-18 21:38:08 +0530

    Update Login Form

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,41 +1,49 @@
 import React, { useState } from "react";
-import "../Style/SignUpForm.css"
+import "../Style/SignUpForm.css";
 import { Link } from "react-router-dom";
-import { app } from '../firebase';
+import { app } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
 
-
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
-
-const handleSubmit = async (e) =>{
-  e.preventDefault()
-  try{
-     await createUserWithEmailAndPassword(auth, email , password)
-     console.log("Login Successfully")
-  }catch(err){
-    console.log(err)
-  }
-}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login Successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="signup-container">
-      <form className="signup-form" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <label htmlFor="email">
-          Email:
-          <input type="text" onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <label htmlFor="password">
-      Password:
-      <input type="password" onChange={(e) => setPassword(e.target.value)}/>
-        </label>
-        <button type="submit">Login</button>
-        <p>Don't get have an account?<Link to = "/signup">Register</Link></p>
-      </form>
+      <div className="signup-sides">
+        <div className="signup-left">
+          <form className="signup-form" onSubmit={handleSubmit}>
+            <h2>Login</h2>
+            <label htmlFor="email">
+              Email:
+              <input type="text" onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+        <div className="signup-right">
+          <h2>Don't have an account?</h2>
+          <Link className="login-link" to="/signup">Register</Link>
+        </div>
+      </div>
     </div>
   );
 };
